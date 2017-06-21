280163e61536626026c4f20f99a957317643bbe3
Gustavo Merling <gustavomerling@gmail.com>
2017-06-21 19:04:35 -0300

    scripts

--- estetica/js/scripts.js
+++ estetica/js/scripts.js
@@ -2,8 +2,24 @@
 // SCRIPTS
 //
 
-$(document).ready(function(){
+function checkAnchors(){
+	$('.anchor').each(function(){
+		if( $(this).offset().top - 200 <= $(window).scrollTop() ){
+			$('.anchor_button').removeClass('active');
+			$('.anchor_button[data-index='+$(this).data('index')+']').addClass('active');
+		}
+	});
+}
 
+$(document).ready(function(){
 	
+	checkAnchors();
+
+	$(window).scroll(function(){checkAnchors();});
+
+	$('.anchor_button').click(function(){
+		var index = $(this).data('index');
+		$('body,html').animate({ 'scrollTop' : $('.anchor[data-index='+index+']').offset().top - 80 } , 800 );
+	});
 	
 });
